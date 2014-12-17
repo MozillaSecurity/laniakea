d7ba1feaa8c7d1eeed7a72691cf2d697bc9e9115
Christoph Diehl <cdiehl@mozilla.com>
2014-12-17 14:41:09 +0100

    Fix accidental removal of tar command

--- user_data/peach.sh
+++ user_data/peach.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+# https://help.ubuntu.com/community/CloudInit
+# http://www.knowceantech.com/2014/03/amazon-cloud-bootstrap-with-userdata-cloudinit-github-puppet/
 set -e -x
 
 # Essential Packages
 apt-get --yes --quiet update
+apt-get --yes --quiet build-dep firefox 
 apt-get --yes --quiet install python python-pip python-dev git
 apt-get --yes --quiet install libxml2-dev libxslt1-dev lib32z1-dev # Peach: lxml
 apt-get --yes --quiet install s3cmd # Peach: userdata.py
+apt-get --yes --quiet install xterm # Peach: agent.py / xterm
 
 # -----------------------------------------------------------------------------
 
@@ -52,6 +56,14 @@ chmod 600 /root/.ssh/id_rsa.pits
 
 # -----------------------------------------------------------------------------
 
+
+cd /home/ubuntu
+
+# Download Target
+wget -r --no-parent -A firefox-*.en-US.linux-x86_64-asan.tar.bz2 https://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/
+wget -r --no-parent -A firefox-*.txt https://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/
+tar xvfj ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/*.tar.bz2
+
 # Checkout Fuzzer
 git clone -v --depth 1 git@peach:MozillaSecurity/peach.git
 cd peach
@@ -59,16 +71,5 @@ git clone -v --depth 1 git@pits:MozillaSecurity/pits.git Pits
 pip -q install -r requirements.txt
 python scripts/userdata.py -sync
 
-# Download Firefox
-wget --no-check-certificate -r --no-parent -A firefox-*.en-US.linux-x86_64-asan.tar.bz2 https://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/
-#wget --no-check-certificate -r --no-parent -A firefox-*.txt https://ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/
-tar xvfj ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest/*.tar.bz2
-
 # Run FuzzingBot
-xvfb-run ./scripts/peachbot.py -data .
-
-# -----------------------------------------------------------------------------
-
-# Stop Instance
-# shutdown -h now
-
+xvfb-run ./scripts/peachbot.py -data . -pits Pits/
