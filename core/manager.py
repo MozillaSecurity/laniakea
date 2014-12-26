a6b23681b40ff2a42c764ba3b99c2f6503aa1467
Christoph Diehl <cdiehl@mozilla.com>
2014-12-26 16:02:23 +0100

    Add missing import statement

--- core/manager.py
+++ core/manager.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import time
+import logging
 
 from boto.ec2 import connect_to_region
 
