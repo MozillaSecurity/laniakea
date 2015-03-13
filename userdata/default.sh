eb0350509b4d2ab52562e60d62e8d7a282c8c702
Christoph Diehl <cdiehl@mozilla.com>
2015-03-13 21:08:31 +0500

    Improve UserData examples for Peach

--- userdata/default.sh
+++ userdata/default.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
-set -e -x
+#!/bin/bash -ex
 
 # Essentials
 apt-get --yes --quiet update
 apt-get --yes --quiet install python python-pip python-dev git puppet-common build-essential
 
 # Fetch puppet configuration
-mv /etc/puppet /etc/puppet.orig
-git clone https://bitbucket.org/rimey/hello-ec2-puppetboot.git /etc/puppet
+# mv /etc/puppet /etc/puppet.orig
+# git clone https://bitbucket.org/rimey/hello-ec2-puppetboot.git /etc/puppet
 
 # Run puppet
-puppet apply /etc/puppet/manifests/init.pp
+# puppet apply /etc/puppet/manifests/init.pp
 
 # Run fuzzers
--- userdata/peach.sh
+++ userdata/peach.sh
@@ -1,15 +1,25 @@
-#!/bin/sh
-# https://help.ubuntu.com/community/CloudInit
-# http://www.knowceantech.com/2014/03/amazon-cloud-bootstrap-with-userdata-cloudinit-github-puppet/
-set -e -x
+#!/bin/bash -ex
+
+@import(userdata/common.sh)@
 
 # Essential Packages
-apt-get --yes --quiet update
-apt-get --yes --quiet build-dep firefox 
-apt-get --yes --quiet install python python-pip python-dev git
-apt-get --yes --quiet install libxml2-dev libxslt1-dev lib32z1-dev # Peach: lxml
-apt-get --yes --quiet install s3cmd # Peach: userdata.py
-apt-get --yes --quiet install xterm # Peach: agent.py / xterm
+
+# The following packages are part of the FuzzingOS base image:
+#sudo apt-get --yes --quiet update
+#sudo apt-get --yes --quiet upgrade
+#sudo apt-get --yes --quiet build-dep firefox
+#sudo apt-get --yes --quiet install \
+#	python python-pip python-dev git mercurial s3cmd
+
+# Peach
+#sudo apt-get --yes --quiet install \
+#	libxml2-dev libxslt1-dev lib32z1-dev xterm
+#sudo pip install \
+#	Twisted==14.0.0 lxml==3.3.5 psutil==2.1.1 pyasn1==0.1.7 tlslite==0.4.6 
+
+# FuzzManager
+#sudo pip install \
+#	Django==1.7.1 numpy==1.9.1 djangorestframework==2.4.4 requests>=2.5.0 lockfile>=0.8
 
 # -----------------------------------------------------------------------------
 
@@ -18,22 +28,16 @@ ssh-keyscan github.com >> /root/.ssh/known_hosts
 
 # Add this key as deploy key to the GitHub project
 # Command: ssh-keygen -t rsa -C "Deploy key for Peach"
-cat << EOF > /root/.ssh/id_rsa.peach.pub
-INSERT_PUBLIC_KEY_HERE
-EOF
-
 cat << EOF > /root/.ssh/id_rsa.peach
-INSERT_PRIVATE_KEY_HERE
+-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
 EOF
 
 # Add this key as deploy key to the GitHub project
 # Command: ssh-keygen -t rsa -C "Deploy key for Pits"
-cat << EOF > /root/.ssh/id_rsa.pits.pub
-INSERT_PUBLIC_KEY_HERE
-EOF
-
 cat << EOF > /root/.ssh/id_rsa.pits
-INSERT_PRIVATE_KEY_HERE
+-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
 EOF
 
 # Setup Key Indentities
@@ -51,31 +55,56 @@ IdentityFile /root/.ssh/id_rsa.pits
 EOF
 
 # Set Key Permissions
-chmod 600 /root/.ssh/id_rsa.peach
-chmod 600 /root/.ssh/id_rsa.pits
+chmod 600 /root/.ssh/id_rsa.*
+
 
 # -----------------------------------------------------------------------------
 
 cd /home/ubuntu
 
-# Download Target
-TARGET_LOCATION="ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/mozilla-inbound-linux64-asan/latest"
-TARGET_URL="ftp://"$TARGET_LOCATION
-wget --force-directories --no-parent --glob=on $TARGET_URL/firefox-*.en-US.linux-x86_64-asan.tar.bz2
-wget --force-directories --no-parent --glob=on $TARGET_URL/*.txt -O $TARGET_LOCATION/revision.txt
-tar xvfj $TARGET_LOCATION/firefox-*.en-US.linux-x86_64-asan.tar.bz2
-TARGET_VERSION=`cat $TARGET_LOCATION/revision.txt`
+# Target desscription for Firefox
+TARGET_PRODUCT="mozilla-inbound-linux64-asan"
+TARGET_LOCATION="ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/${TARGET_PRODUCT}/latest"
+TARGET_URL="ftp://${TARGET_LOCATION}"
+retry wget --force-directories --no-parent --glob=on ${TARGET_URL}/firefox-*.en-US.linux-x86_64-asan.tar.bz2
+retry wget --force-directories --no-parent --glob=on ${TARGET_URL}/*.txt -O ${TARGET_LOCATION}/revision.txt
+tar xvfj ${TARGET_LOCATION}/firefox-*.en-US.linux-x86_64-asan.tar.bz2
+TARGET_VERSION=`cat ${TARGET_LOCATION}/revision.txt`
 
-# Checkout Fuzzer
-git clone -v --depth 1 git@peach:MozillaSecurity/peach.git
+# Checkout Peach
+retry git clone -v --depth 1 git@peach:MozillaSecurity/peach.git
 cd peach
-git clone -v --depth 1 git@pits:MozillaSecurity/pits.git Pits
+retry git clone -v --depth 1 git@pits:MozillaSecurity/pits.git Pits
 pip -q install -r requirements.txt
-python scripts/userdata.py -sync
+retry python scripts/userdata.py -sync
 
 # Checkout and setup FuzzManager
-git clone -v --depth 1 https://github.com/MozillaSecurity/FuzzManager.git Peach/Utilities/FuzzManager
+retry git clone -v --depth 1 https://github.com/MozillaSecurity/FuzzManager.git Peach/Utilities/FuzzManager
 pip install -r Peach/Utilities/FuzzManager/requirements.txt
 
-# Run FuzzingBot
-xvfb-run ./scripts/peachbot.py -tasks 50 -data . -pits Pits/ -macros FuzzManagerServerAuthToken=41a4d5fd011f058ae32d4d71f99a45457e891d86 FuzzManagerTargetVersion=$TARGET_VERSION
+# Create base FuzzManager configuration
+cat > /home/ubuntu/.fuzzmanagerconf << EOL
+[Main]
+serverhost = darpa.spdns.de
+serverport = 8000
+serverproto = http
+serverauthtoken = @AUTH_TOKEN@
+sigdir = /home/ubuntu/signatures
+EOL
+echo "clientid =" `curl --retry 5 -s http://169.254.169.254/latest/meta-data/public-hostname` >> /home/ubuntu/.fuzzmanagerconf
+
+# Create binary FuzzManager configuration
+cat > /home/ubuntu/firefox/firefox.fuzzmanagerconf << EOL
+[Main]
+platform = x86-64
+product = ${TARGET_PRODUCT}
+product_version = ${TARGET_VERSION}
+os = `uname -s`
+EOL
+
+# Ensure proper permissions
+chown -R ubuntu:ubuntu /home/ubuntu
+
+# Run FuzzingBot as user "ubuntu"
+#su -c "xvfb-run ./scripts/peachbot.py -tasks 50 -testcases 50000 -data . -pits Pits/" ubuntu
+su -c "screen -t peach -dmS peach xvfb-run ./scripts/peachbot.py -tasks 50 -testcases 50000 -data . -pits Pits/" ubuntu
