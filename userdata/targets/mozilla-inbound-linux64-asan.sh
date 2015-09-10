1ef15e28590d3f47a0d2a61edabdb361b4a5d354
Christoph Diehl <cdiehl@mozilla.com>
2015-09-11 01:49:47 +0200

    Switch from FTP to HTTPS for downloading from ftp.mozilla.org

--- userdata/targets/mozilla-inbound-linux64-asan.sh
+++ userdata/targets/mozilla-inbound-linux64-asan.sh
@@ -1,8 +1,8 @@
 # Target desscription for Firefox
 TARGET_PRODUCT="mozilla-inbound-linux64-asan"
 TARGET_LOCATION="ftp.mozilla.org/pub/mozilla.org/firefox/tinderbox-builds/${TARGET_PRODUCT}/latest"
-TARGET_URL="ftp://${TARGET_LOCATION}"
-retry wget --force-directories --no-parent --glob=on "${TARGET_URL}/firefox-*.en-US.linux-x86_64-asan.tar.bz2"
-retry wget --force-directories --no-parent --glob=on "${TARGET_URL}/firefox-*-asan.json"
+TARGET_URL="https://${TARGET_LOCATION}"
+retry wget -r -l1 -np -A "firefox-*.en-US.linux-x86_64-asan.tar.bz2" "${TARGET_URL}"
+retry wget -r -l1 -np -A "firefox-*-asan.json" "${TARGET_URL}"
 tar xvfj ${TARGET_LOCATION}/firefox-*.en-US.linux-x86_64-asan.tar.bz2
 TARGET_VERSION=`cat ${TARGET_LOCATION}/firefox-*.json | python -c 'import sys,json; print(json.load(sys.stdin)["moz_source_stamp"])'`
