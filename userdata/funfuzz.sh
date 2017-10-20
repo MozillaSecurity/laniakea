2f0690800ad796a34c9596f456f2b656210d33a4
Gary Kwong <gary@rumblingedge.com>
2017-10-20 14:44:08 -0700

    2017-10 general updates for userdata/funfuzz.sh

--- userdata/funfuzz.sh
+++ userdata/funfuzz.sh
@@ -1,6 +1,6 @@
 #! /bin/bash -ex
 # Be in ~/trees/laniakea directory, be sure @import directories are present.
-# ~/trees/boto-awsfuzz/bin/python -u ~/trees/laniakea/laniakea.py -region=us-east-1 -images ~/images.json -create-on-demand -tags Name=funfuzz-1604-ondemand-201708 -image-name funfuzz-ondemand-ebs -ebs-volume-delete-on-termination -ebs-size 96 -root-device-type ebs -userdata userdata/funfuzz.sh
+# ~/trees/boto-awsfuzz/bin/python -u ~/trees/laniakea/laniakea.py -region=us-east-1 -images ~/images.json -create-on-demand -tags Name=funfuzz-1604-ondemand-201710 -image-name funfuzz-ondemand-ebs -ebs-volume-delete-on-termination -ebs-size 96 -root-device-type ebs -userdata userdata/funfuzz.sh
 # Stop the instance, create an AMI, copy the AMI, then update EC2SpotManager
 export DEBIAN_FRONTEND=noninteractive  # Bypass ncurses configuration screens
 
@@ -10,8 +10,8 @@ sleep 10  # EC2 takes some time to be able to go online
 add-apt-repository -y ppa:git-core/ppa  # Git PPA needed to get latest security updates
 apt-get --yes --quiet update
 apt-get --yes --quiet dist-upgrade
-# Check using `hg --cwd ~/trees/mozilla-central/ diff -r bfc7b187005c:56188620cce0 python/mozboot/mozboot/debian.py`
-# Retrieved on 2017-08-25: https://hg.mozilla.org/mozilla-central/file/56188620cce0/python/mozboot/mozboot/debian.py
+# Check using `hg --cwd ~/trees/mozilla-central/ diff -r 56188620cce0:b6ba3e919f56 python/mozboot/mozboot/debian.py`
+# Retrieved on 2017-10-19: https://hg.mozilla.org/mozilla-central/file/b6ba3e919f56/python/mozboot/mozboot/debian.py
 apt-get --yes --quiet install autoconf2.13 build-essential ccache python-dev python-pip python-setuptools unzip uuid zip
 apt-get --yes --quiet install libasound2-dev libcurl4-openssl-dev libdbus-1-dev libdbus-glib-1-dev libgconf2-dev
 apt-get --yes --quiet install libgtk2.0-dev libgtk-3-dev libiw-dev libnotify-dev libpulse-dev libx11-xcb-dev libxt-dev
@@ -19,7 +19,7 @@ apt-get --yes --quiet install mesa-common-dev python-dbus yasm xvfb
 apt-get --yes --quiet install cmake curl gdb git openssh-client openssh-server screen silversearcher-ag vim
 apt-get --yes --quiet install lib32z1 gcc-multilib g++-multilib  # For compiling 32-bit in 64-bit OS
 # Needed for Valgrind and for compiling with clang, along with llvm-symbolizer
-apt-get --yes --quiet install valgrind libc6-dbg clang
+apt-get --yes --quiet install valgrind libc6-dbg
 
 # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
@@ -60,7 +60,7 @@ sudo -u ubuntu git clone https://github.com/MozillaSecurity/funfuzz /home/ubuntu
 sudo -u ubuntu git clone https://github.com/MozillaSecurity/FuzzManager /home/ubuntu/FuzzManager
 
 # Get more fuzzing prerequisites
-pip install --upgrade pip
+pip install --upgrade pip setuptools
 pip install --upgrade virtualenv mercurial
 
 # Get supporting fuzzing libraries via pip
