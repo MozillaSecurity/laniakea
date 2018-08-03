5fb56d1d9091f02609e3b45c9d42bd9a6356fc6f
posidron <1614333+posidron@users.noreply.github.com>
2018-08-03 21:51:23 +0200

    Add test construct for Packet

---
+++ tests/test_packet.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+Tests for Packet.
+"""
+import os
+import json
+import pytest
+
+from laniakea.core.providers.packet import PacketManager
+
+@pytest.fixture
+def packet():
+    with open(os.path.join(os.getcwd(), 'laniakea/examples/packet.json')) as fo:
+        conf = json.loads(fo.read())
+    return PacketManager(conf)
+
+def test_list_projects(packet):
+    for plan in packet.list_projects():
+        assert hasattr(plan, 'name')
+        assert hasattr(plan, 'id')
