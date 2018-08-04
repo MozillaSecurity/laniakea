0250e472dcc4c5ad3845df06623e00fbfc13bf58
posidron <1614333+posidron@users.noreply.github.com>
2018-08-04 13:24:59 +0200

    Remove rotten userdata scripts

--- laniakea/userdata/ec2/loggers/fuzzmanager.binary.sh
+++
@@ -1,8 +0,0 @@
-# Create binary FuzzManager configuration
-cat > /home/ubuntu/firefox/firefox.fuzzmanagerconf << EOL
-[Main]
-platform = x86-64
-product = ${TARGET_PRODUCT}
-product_version = ${TARGET_VERSION}
-os = `uname -s`
-EOL
