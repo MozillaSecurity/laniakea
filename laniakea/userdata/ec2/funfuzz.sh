37ddef4464023761ceabea30a2a53ae4ba700a4e
Gary Kwong <nth10sd@gmail.com>
2018-10-30 21:31:50 -0700

    .vimrc updates for funfuzz.sh

--- laniakea/userdata/ec2/funfuzz.sh
+++ laniakea/userdata/ec2/funfuzz.sh
@@ -117,6 +117,24 @@ chown ubuntu:ubuntu /home/ubuntu/.hgrc
 
 # Add vimrc for Bionic
 cat << EOF > /home/ubuntu/.vimrc
+:syntax enable
+syntax on
+set number
+set ruler
+set nocompatible
+set bs=2
+fixdel
+set nowrap
+set tabstop=4
+set autoindent
+set term=xterm
+set smartindent
+set showmode showcmd
+set shiftwidth=4
+set expandtab
+set backspace=indent,eol,start
+set hls
+au BufNewFile,BufRead *.* exec 'match Error /\%119v/'
 set paste
 EOF
 
