4b9659b1a3a567f7e0d20ed58fc1d29e8cbf1621
posidron <1614333+posidron@users.noreply.github.com>
2019-06-01 18:02:56 +0200

    Bump version to 1.7.1

--- laniakea/__version__.py
+++ laniakea/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'laniakea'
-__version__ = '1.17.0'
+__version__ = '1.17.1'
 __license__ = 'MPL 2.0'
 __description__ = 'Laniakea is a utility for managing instances at various cloud providers and \
 aids in setting up a fuzzing cluster it can be used as a tool or as a library.'
